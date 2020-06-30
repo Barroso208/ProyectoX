ef8c7c74333f87f0d7bbbab14849c8abd960c337
Barroso208 <Barroso208@users.noreply.github.com>
2020-06-30 15:17:08 +0200

    animaciones-2

--- scripts/script-1-menu.js
+++ scripts/script-1-menu.js
@@ -3,10 +3,14 @@ let linksContainer = document.getElementById("links-container");
 let linkp = document.getElementById("link-1");
 let links = document.getElementById("link-2");
 let linkt = document.getElementById("link-3");
+let h1    = document.getElementById("header-div1-h1");
+let h3    = document.getElementById("header-div1-h3");
 function menu(){
     if(window.screen.width<1090){
         if(linksContainer.className == "links-none"){
             linksContainer.setAttribute("class","links-container");
+            h1.style.position ="unset";
+            h3.style.position ="unset";
         }else {
             linksContainer.setAttribute("class","links-none");
         }
