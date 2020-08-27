43011ab8f2b7e884afd8660571c4fa39f6ee741d
Barroso208 <Barroso208@users.noreply.github.com>
2020-08-27 16:08:55 +0200

    añadiendo una animacion al menu

--- scripts/script-1-menu.js
+++ scripts/script-1-menu.js
@@ -5,17 +5,17 @@ const h3    = document.getElementById("header-div1-h3");
 function menu(){
     if(window.screen.width<1090){
         if(linksContainer.className == "links-none"){
-            linksContainer.setAttribute("class","links-container");
             h1.style.position ="unset";
             h3.style.position ="unset";
+            linksContainer.setAttribute("class","links-container");
         }else {
-            linksContainer.setAttribute("class","links-none");
+            $('div.links-container').animate({opacity:0.0},500,function() {
+                linksContainer.setAttribute("class","links-none");
+                linksContainer.style.opacity = 100;
+            });
         }
     }
 }
 $('#menu').on('click',menu);
-$('div.links-none').on('click',function(e){
-    menu();
-});
-
+$('div.links-none').on('click',menu);
 document.getElementById("last").textContent = "Ultima modificación: " + document.lastModified;
